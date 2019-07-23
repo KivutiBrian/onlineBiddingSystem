19fd94c260c8f76045eda0491292329599965fed
kivutibrian <briankivuti1@gmail.com>
2019-07-23 09:03:44 +0300

    first commit

---
+++ config/config.py
@@ -0,0 +1,11 @@
+class Config(object):
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Brian8053@@127.0.0.1:5432/onlineBiddingSystem'
+    SECRET_KEY = 'some=secret-key'
+
+class ProductionConfig(Config):
+    DEBUG = False
